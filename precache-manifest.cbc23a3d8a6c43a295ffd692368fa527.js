ffa1d71ef2a21b6e28b063bdd489828ea8daf96f
arteboris <arteboris@ukr.net>
2020-02-28 15:33:31 +0200

    Updates

---
+++ precache-manifest.cbc23a3d8a6c43a295ffd692368fa527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21800dce537530b47f012d1b923913b",
+    "url": "/device-tree/index.html"
+  },
+  {
+    "revision": "43748e476f288379ca40",
+    "url": "/device-tree/static/css/main.cb49337a.chunk.css"
+  },
+  {
+    "revision": "e16b1205c5c9c5a25207",
+    "url": "/device-tree/static/js/2.5e818e68.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/device-tree/static/js/2.5e818e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43748e476f288379ca40",
+    "url": "/device-tree/static/js/main.89437558.chunk.js"
+  },
+  {
+    "revision": "150d4f45fe3253ee1175",
+    "url": "/device-tree/static/js/runtime-main.7f6269c0.js"
+  }
+]);
